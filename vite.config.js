38789943e55fb85e326fab7a047f0cb25c676777
thejakumari <theja.xworkz@gmail.com>
2024-07-04 11:15:55 +0530

    first

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.VITE_BASE_URL || '/',
+  build: {
+    outDir: 'dist', // Ensure this is correctly set to 'dist' for outputting files to /dist directory
+  },
 })
